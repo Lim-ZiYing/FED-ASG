91c7b4203677f49f82eb9429c30e9a820beab347
Siyu528 <ge82475@163.com>
2026-02-06 21:11:18 +0800

    firebase

---
+++ Feature two/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAL8H-6QQcdpIKnZyngdVYnBjSXNdYaCKE",
+  authDomain: "fed-assignment-607f5.firebaseapp.com",
+  projectId: "fed-assignment-607f5",
+  storageBucket: "fed-assignment-607f5.firebasestorage.app",
+  messagingSenderId: "865290865679",
+  appId: "1:865290865679:web:fa9903ebfb2705f3b8a77a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore DB
+export const db = getFirestore(app);

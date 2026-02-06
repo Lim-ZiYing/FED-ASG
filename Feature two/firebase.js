74787afec624d487256c1056e11f283fba19efd6
Siyu528 <ge82475@163.com>
2026-02-06 22:34:27 +0800

    firebase

--- Feature two/firebase.js
+++ Feature two/firebase.js
@@ -1,16 +1,16 @@
-// Feature two/firebase.js (CONFIG / COMPAT version)
 
 const firebaseConfig = {
   apiKey: "AIzaSyAL8H-6QQcdpIKnZyngdVYnBjSXNdYaCKE",
   authDomain: "fed-assignment-607f5.firebaseapp.com",
   projectId: "fed-assignment-607f5",
-  storageBucket: "fed-assignment-607f5.appspot.com",
+  storageBucket: "fed-assignment-607f5.firebasestorage.app",
   messagingSenderId: "865290865679",
-  appId: "1:865290865679:web:fa9903ebfb2705f3b8a77a"
+  appId: "1:865290865679:web:fa9903ebfb2705f3b8a77a",
+  measurementId: "G-CH6JWEQ7E2"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Firestore reference (GLOBAL)
+// Initialize Firestore
 const db = firebase.firestore();
